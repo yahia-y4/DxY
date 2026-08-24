5382c9304821b7227e476e64c40a230d4090936c
yahia-y4 <yalhmwd994@gmail.com>
2026-08-24 18:26:23 +0300

    create -add,edit,delete,show- TreatmentPlan funs

--- backend/app/Http/Controllers/TreatmentPlanController.php
+++ backend/app/Http/Controllers/TreatmentPlanController.php
@@ -3,8 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TreatmentPlan;
+
 
 class TreatmentPlanController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $doctorId = $request->user()->id;
+        $treatmentPlans = TreatmentPlan::where('doctor_id', $doctorId)->get();
+        return response()->json(['treatment_plans' => $treatmentPlans], 200);
+    }
+    public function store(Request $request)
+    {
+
+        $treatmentPlan = new TreatmentPlan();
+        $treatmentPlan->doctor_id = $request->user()->id;
+        $treatmentPlan->patient_id = $request->patient_id;
+        $treatmentPlan->name = $request->name;
+        $treatmentPlan->description = $request->description;
+        $treatmentPlan->save();
+        return response()->json(['message' => 'Treatment plan created successfully', 'treatment_plan' => $treatmentPlan], 201);
+
+    }
+
+    public function edit($id , Request $request)
+    {
+        $treatmentPlan = TreatmentPlan::find($id);
+        if (!$treatmentPlan) {
+            return response()->json(['message' => 'Treatment plan not found'], 404);
+        }
+        $treatmentPlan->name = $request->name;
+        $treatmentPlan->description = $request->description;
+        $treatmentPlan->save();
+        return response()->json(['message' => 'Treatment plan updated successfully', 'treatment_plan' => $treatmentPlan], 200);
+    }
+
+    public function delete($id)
+    {
+        $treatmentPlan = TreatmentPlan::find($id);
+        if (!$treatmentPlan) {
+            return response()->json(['message' => 'Treatment plan not found'], 404);
+        }
+        $treatmentPlan->delete();
+        return response()->json(['message' => 'Treatment plan deleted successfully'], 200);
+    }
+    
+      
+
+
+
 }
---
+++ backend/routes/treatmentPlans.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TreatmentPlanController;
+
+Route::middleware('auth:sanctum')->group(function () {
+   
+    Route::get('/treatmentPlans', [TreatmentPlanController::class, 'index']);
+    Route::post('/treatmentPlans/store', [TreatmentPlanController::class, 'store']);
+    Route::put('/treatmentPlans/edit/{id}', [TreatmentPlanController::class, 'edit']);
+    Route::delete('/treatmentPlans/delete/{id}', [TreatmentPlanController::class, 'delete']);
+});
