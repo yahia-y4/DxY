9343e27a6175e5ba5691a390b7ee20fad6162e97
yahia-y4 <yalhmwd994@gmail.com>
2026-09-01 19:51:56 +0300

    Relationships in patien TreatmentSession  model

--- backend/app/Models/TreatmentPlan.php
+++ backend/app/Models/TreatmentPlan.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class TreatmentPlan extends Model
 {
-    //
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'name',
+        'description',
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
--- backend/app/Models/TreatmentSession.php
+++ backend/app/Models/TreatmentSession.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class TreatmentSession extends Model
 {
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function treatmentPlan()
+    {
+        return $this->belongsTo(TreatmentPlan::class);
+    }
 public function patient()
 {
     return $this->belongsTo(Patient::class);
