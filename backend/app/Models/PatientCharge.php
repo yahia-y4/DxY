dfc0c05c392d31ac3ab51f637643ecedfffff7f3
yahia-y4 <yalhmwd994@gmail.com>
2026-09-01 19:25:12 +0300

    Relationships in patientCharge  model

--- backend/app/Models/PatientCharge.php
+++ backend/app/Models/PatientCharge.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class PatientCharge extends Model
 {
     //
+
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'amount',
+        'note',
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+    
 }
