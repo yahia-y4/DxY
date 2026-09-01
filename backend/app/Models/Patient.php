a49fb1395a25feb3e36f706a6ba74c171d4e9aa1
yahia-y4 <yalhmwd994@gmail.com>
2026-09-01 19:16:57 +0300

    Relationships in patient model

--- backend/app/Models/Patient.php
+++ backend/app/Models/Patient.php
@@ -6,8 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'outstanding_balance',
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
     public function sessions()
-{
+   {
     return $this->hasMany(TreatmentSession::class);
-}
+   }
+   
+    public function charges()
+    {
+        return $this->hasMany(PatientCharge::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(PatientPayment::class);
+    }
+
+    
 }
