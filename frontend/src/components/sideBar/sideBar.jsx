4e077a0c319d26ea39dcf54d9b6f843aaf735b0c
yahia-y4 <yalhmwd994@gmail.com>
2026-09-10 12:44:21 +0300

    side bar buts style 2

--- frontend/src/components/sideBar/sideBar.jsx
+++ frontend/src/components/sideBar/sideBar.jsx
@@ -2,17 +2,26 @@
 import SideBarBut from "../sideBarBut/sideBarBut"
 import "./sideBar.css"
 
+
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PersonalInjuryOutlinedIcon from '@mui/icons-material/PersonalInjuryOutlined';
+import AirlineSeatFlatAngledOutlinedIcon from '@mui/icons-material/AirlineSeatFlatAngledOutlined';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
+import PollOutlinedIcon from '@mui/icons-material/PollOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 export default function SideBar() {
   return (
     <div className="sideBar">
-      <SideBarBut icon="🏠" text="الرئيسية" />
-      <SideBarBut icon="🏠" text="المرضى" />
-      <SideBarBut icon="🏠" text="الجلسات" />
-      <SideBarBut icon="🏠" text="الخطط العلاجية" />
-      <SideBarBut icon="🏠" text="المواعيد" />
-      <SideBarBut icon="🏠" text="المالية" />
-      <SideBarBut icon="🏠" text="الاحصائيات" />
-      <SideBarBut icon="🏠" text="الحساب" />
+      <SideBarBut icon={<HomeOutlinedIcon/>} text="الرئيسية" />
+      <SideBarBut icon={<PersonalInjuryOutlinedIcon/>} text="المرضى" />
+      <SideBarBut icon={<AirlineSeatFlatAngledOutlinedIcon/>} text="الجلسات" />
+      <SideBarBut icon={<LibraryBooksOutlinedIcon/>} text="الخطط العلاجية" />
+      <SideBarBut icon=<AssignmentOutlinedIcon/> text="المواعيد" />
+      <SideBarBut icon=<AttachMoneyOutlinedIcon/> text="المالية" />
+      <SideBarBut icon=<PollOutlinedIcon/> text="الاحصائيات" />
+      <SideBarBut icon=<PersonOutlineOutlinedIcon/> text="الحساب" />
     </div>
   )
 }
