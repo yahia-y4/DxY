5d837be4dba24a0e5aed0a8cfb175944d6d5a23c
yahia-y4 <yalhmwd994@gmail.com>
2026-09-18 14:45:01 +0300

    financeAdd  ui

--- frontend/src/modules/finance/financePage/financePage.jsx
+++ frontend/src/modules/finance/financePage/financePage.jsx
@@ -1,10 +1,12 @@
 
+import FinanceAdd from "../sections/financeAdd/financeAdd"
 import FinanceTable from "../sections/financeTable/financeTable"
 import "./financePage.css"
 
 export default function FinancePage(){
     return(
         <div className="financePage">
+            <FinanceAdd/>
             <FinanceTable/>
         </div>
     )
---
+++ frontend/src/modules/finance/sections/financeAdd/financeAdd.jsx
@@ -0,0 +1,24 @@
+
+import "./financeAdd.css"
+import ListButton from "../../../../components/listButton/listButton"
+import Input from "../../../../components/input/input"
+import Select from "../../../../components/select/select"
+import Button from "../../../../components/button/button"
+export default function FinanceAdd(){
+    return(
+        <div className="financeAdd">
+            <div className="financeAdd-Control">
+                <ListButton label={"اضافة دين"}/>
+                <ListButton label={"استلام دفعة"}/>
+            </div>
+            <Select label={"المريض"}/>
+            <Input type="number" label={"المبلغ"}/>
+            <Input label={"ملاحظة"}/>
+
+             <div className="financeAdd-buts">
+                <Button lable={"اضافة"}/>
+                <Button lable={"الغاء"}/>
+             </div>
+        </div>
+    )
+}
