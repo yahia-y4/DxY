bf853006f349dd3d3a218e604e71c1a8197847d6
yahia-y4 <yalhmwd994@gmail.com>
2026-09-21 19:11:12 +0300

    init data in sessions table

--- frontend/src/modules/sessions/sections/sessionsTable/sessionsTable.jsx
+++ frontend/src/modules/sessions/sections/sessionsTable/sessionsTable.jsx
@@ -5,15 +5,22 @@ import Search from "../../../../components/search/search";
 export default function SectionsTable() {
   const columns = [
     { name: "id", label: "ID" },
-    { name: "name", label: "Name" },
-    { name: "age", label: "Age" },
-    { name: "gender", label: "Gender" },
+    { name: "name", label: "الاسم" },
+    { name: "father_name", label: "الاب" },
+    { name: "nick_name", label: "الكنية" },
+    { name: "sections_name", label: "الجلسة" },
+    { name: "date", label: "تاريخ الجلسة" },
+ 
   ];
   const data = [
-    { id: 1, name: "John Doe", age: 30, gender: "Male" },
-    { id: 2, name: "Jane Smith", age: 25, gender: "Female" },
-    { id: 3, name: "Alice Johnson", age: 28, gender: "Female" },
+    {id: 1, name: "محمد", father_name: "احمد", nick_name: "ابو احمد", sections_name: "جلسة علاج طبيعي", date: "2023-01-01"},
+    {id: 2, name: "علي", father_name: "حسن", nick_name: "ابو حسن", sections_name: "جلسة علاج طبيعي", date: "2023-01-02"},
+    {id: 3, name: "سارة", father_name: "محمد", nick_name: "ام محمد", sections_name: "جلسة علاج طبيعي", date: "2023-01-03"},
+    {id: 4, name: "أحمد", father_name: "علي", nick_name: "ابو علي", sections_name: "جلسة علاج طبيعي", date: "2023-01-04"},
+    {id: 5, name: "فاطمة", father_name: "حسن", nick_name: "ام حسن", sections_name: "جلسة علاج طبيعي", date: "2023-01-05"},
   ];
+  
+ 
   return (
     <div className="sessionsTable">
       <Search w={"90%"} />
--- frontend/src/modules/sessions/sessionsPage/sessionsPage.jsx
+++ frontend/src/modules/sessions/sessionsPage/sessionsPage.jsx
@@ -1,16 +1,16 @@
 
 // import SessionAdd from "../sections/sessionAdd/sessionAdd"
-import SessionInfo from "../sections/sessionInfo/sessionInfo"
+// import SessionInfo from "../sections/sessionInfo/sessionInfo"
 import "./sessionsPage.css"
-//import SectionsTable from "../sections/sessionsTable/sessionsTable"
+import SectionsTable from "../sections/sessionsTable/sessionsTable"
 
 
 export default function SessionsPage(){
     return(
         <div className="sessions-Page">
-        {/* <SectionsTable/> */}
+        <SectionsTable/>
         {/* <SessionAdd/> */}
-        <SessionInfo/>
+        {/* <SessionInfo/> */}
         </div>
     )
 }
