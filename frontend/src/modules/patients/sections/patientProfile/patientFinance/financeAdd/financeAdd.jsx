ef26b9226205a11e26d6a9f0e9508c09e989ddad
yahia-y4 <yalhmwd994@gmail.com>
2026-09-22 10:30:23 +0300

    patient finance back icon  in patient module

--- frontend/src/modules/patients/sections/patientProfile/patientFinance/financeAdd/financeAdd.jsx
+++ frontend/src/modules/patients/sections/patientProfile/patientFinance/financeAdd/financeAdd.jsx
@@ -3,10 +3,23 @@ import "./financeAdd.css"
 import ListButton from "../../../../../../components/listButton/listButton";
 import Input from "../../../../../../components/input/input";
 import Button from "../../../../../../components/button/button";
+import IconButton from "../../../../../../components/iconButton/iconButton";
+import ForwardIcon from '@mui/icons-material/Forward';
+import { handleArrayState } from "../../../../../../helperFunctions/handleArrayState";
+import { PatientsContext } from "../../../../context/patientsContext";
+import { useContext } from "react";
 export default function FinanceAdd(){
+    const {setSelectedPatientSection} = useContext(PatientsContext);
+    //functions
+
+    function back(){
+        handleArrayState(setSelectedPatientSection,1,null)
+    }
+    //
     return(
         <div className="financeAdd">
             <div className="financeAdd-Control">
+                <IconButton onClick={back} icon={<ForwardIcon style={{fontSize:"35px"}} />}/>
                 <ListButton label={"اضافة دين"}/>
                 <ListButton label={"استلام دفعة"}/>
             </div>
