21cf5e794682913404206f1245f550b62b989b18
yahia-y4 <yalhmwd994@gmail.com>
2026-09-22 14:26:19 +0300

    init TanStack Query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
-  </BrowserRouter>
-)
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+import "./index.css";
+import App from "./App.jsx";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>,
+);
