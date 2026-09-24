02166f24fec2a47775d7f615161899f569c99857
yahia-y4 <yalhmwd994@gmail.com>
2026-09-24 10:32:29 +0300

    create Session formState

--- frontend/src/components/select/select.jsx
+++ frontend/src/components/select/select.jsx
@@ -1,11 +1,15 @@
-
-import "./select.css"
-
-export default function Select({label,w,name}){
-    return(
-        <div className="select-div" style={{width:w}}>
-            <label>{label}</label>
-            <select name={name}></select>
-        </div>
-    )
+import "./select.css";
+export default function Select({ label, w, name, patients=[], value, onChange}) {
+  return (
+    <div className="select-div" style={{ width: w }}>
+      <label>{label}</label>
+      <select onChange={(e)=>onChange(e)} name={name} value={value}>
+        {patients.map((patient) => (
+          <option value={patient.id} key={patient.id}>
+            {patient.name} {patient.father_name}  {patient.nick_name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
--- frontend/src/modules/sessions/sections/sessionAdd/sessionAdd.jsx
+++ frontend/src/modules/sessions/sections/sessionAdd/sessionAdd.jsx
@@ -7,6 +7,9 @@ import "./sessionAdd.css";
 import { useSession } from "../../context/useSession";
 import { handleArrayState } from "../../../../helperFunctions/handleArrayState";
 import { chooseToothState } from "../../../../helperFunctions/chooseToothState";
+import {usePatients} from "../../../patients/queries/usePatients";
+import { useQueryUI } from "../../../../hooks/useQueryUI";
+import { handleInputs } from "../../../../helperFunctions/handleInputs";
 import { useState } from "react";
 export default function SessionAdd() {
   const { setCurrentSession } = useSession();
@@ -14,32 +17,32 @@ export default function SessionAdd() {
     patient_id: 1,
     treatment_plan_id: null,
     name:"",
-    description: "description",
-    diagnosis: "diagnosis",
-    treatment: "treatment",
-    prescribed_medication: "prescribed_medication",
-    teeth_number: 4,
-    teeth_vertical: "top",
-    teeth_horizontal: "left",
+    description: "",
+    diagnosis: "",
+    treatment: "",
+    prescribed_medication: "",
+    teeth_number: 0,
+    teeth_vertical: "",
+    teeth_horizontal: "",
   });
 
+  const {data ,isLoading,isError,error,hasToken} = usePatients()
+    useQueryUI({isLoading,isError,error,hasToken})
   //functions
   function back() {
     handleArrayState(setCurrentSession, 0, "sessionsTable");
   }
-
-
-
   function onToothClick(tooth){
     chooseToothState(setFormData,formData,tooth)
   }
+  console.log(formData)
   //
   return (
     <div className="sessionAdd">
       <section className="section-1">
         <form>
-          <Input name={"name"} value={formData.name} label={"اسم الجلسة"} />
-          <Select name={"patient_id"} label={"المريض"} />
+          <Input onChange={(e)=>handleInputs(setFormData,e)} name={"name"} value={formData.name} label={"اسم الجلسة"} />
+          <Select onChange={(e)=>handleInputs(setFormData,e)} patients={data} name={"patient_id"} value={formData.patient_id} label={"المريض"} />
         </form>
         <AddToothWin onToothClick={onToothClick}
           number={formData.teeth_number}
@@ -56,10 +59,10 @@ export default function SessionAdd() {
         </div>
       </section>
       <section className="section-2">
-        <Textarea label={"التشخيص"} h={"150px"} />
-        <Textarea label={"المعالجة"} h={"150px"} />
-        <Textarea label={"الدواء الموصوف"} h={"150px"} />
-        <Textarea label={"ملاحظة"} h={"150px"} />
+        <Textarea onChange={(e)=>handleInputs(setFormData,e)} value={formData.diagnosis} name={"diagnosis"} label={"التشخيص"} h={"150px"} />
+        <Textarea onChange={(e)=>handleInputs(setFormData,e)} value={formData.treatment} name={"treatment"} label={"المعالجة"} h={"150px"} />
+        <Textarea onChange={(e)=>handleInputs(setFormData,e)} value={formData.prescribed_medication} name={"prescribed_medication"} label={"الدواء الموصوف"} h={"150px"} />
+        <Textarea onChange={(e)=>handleInputs(setFormData,e)} value={formData.description} name={"description"} label={"الوصف"} h={"150px"} />
       </section>
     </div>
   );
