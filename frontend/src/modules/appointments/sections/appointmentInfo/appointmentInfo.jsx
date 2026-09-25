e20050e74f15eba5beb427ec6e3659f4a5a6441f
yahia-y4 <yalhmwd994@gmail.com>
2026-09-25 19:24:28 +0300

    show appointment info

--- frontend/src/modules/appointments/sections/appointmentInfo/appointmentInfo.jsx
+++ frontend/src/modules/appointments/sections/appointmentInfo/appointmentInfo.jsx
@@ -4,26 +4,42 @@ import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined
 import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-
-import InfoWin from "../../../../components/infoWin/infoWin"
+import { useAppo } from "../../context/useAppo";
+import InfoWin from "../../../../components/infoWin/infoWin";
+import IconButton from "../../../../components/iconButton/iconButton";
+import { getDayName } from "../../../../helperFunctions/getDayName";
 export default function AppointmentInfo() {
+  const { setAppoSection, selectedAppo } = useAppo();
+
+  //functions
+  function back() {
+    setAppoSection("appoAdd");
+  }
+    function handleStatus(status) {
+    let _status = "انتظار";
+    if (status == "approved") _status = "تم";
+    else if (status == "rejected") _status = "ملغي";
+    return _status;
+  }
+  //-----
   return (
     <div className="appointmentInfo">
       <div className="control-buts">
-        <AppRegistrationOutlinedIcon />
-        <DeleteOutlineOutlinedIcon />
-        <CheckCircleOutlinedIcon />
-        <CancelOutlinedIcon />
-        <ArrowBackIcon />
+        <IconButton icon={<AppRegistrationOutlinedIcon />} />
+        <IconButton icon={<DeleteOutlineOutlinedIcon />} />
+        <IconButton icon={<CheckCircleOutlinedIcon />} />
+        <IconButton icon={<CancelOutlinedIcon />} />
+        <IconButton onClick={back} icon={<ArrowBackIcon />} />
       </div>
       <div className="content">
-        <InfoWin data={"يحيى محمد الحمود"}/>
-        <InfoWin data={"معالجة رحى ثانية لبية"}/>
-        <InfoWin data={"منتظر"}/>
-        <InfoWin data={"2026/9/20"}/>
-        <InfoWin data={"السبت"}/>
-        <InfoWin data={"12:30 PM"}/>
-        
+        <InfoWin
+         data={`${selectedAppo.patient.name} ${selectedAppo.patient.father_name} ${selectedAppo.patient.nick_name}`}
+          />
+        <InfoWin data={selectedAppo.vist_reason} />
+        <InfoWin data={handleStatus(selectedAppo.status)} />
+        <InfoWin data={"2026/9/20"} />
+        <InfoWin data={getDayName(selectedAppo.appointment_date)} />
+        <InfoWin data={selectedAppo.hour} />
       </div>
     </div>
   );
