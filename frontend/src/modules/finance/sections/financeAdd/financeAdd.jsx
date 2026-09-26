a1a1a93d18458a61ca480ba5d2e0e8130ede75f6
yahia-y4 <yalhmwd994@gmail.com>
2026-09-26 21:59:26 +0300

    show  charges and payment in table done

--- frontend/src/modules/finance/sections/financeAdd/financeAdd.jsx
+++ frontend/src/modules/finance/sections/financeAdd/financeAdd.jsx
@@ -4,14 +4,16 @@ import ListButton from "../../../../components/listButton/listButton"
 import Input from "../../../../components/input/input"
 import Select from "../../../../components/select/select"
 import Button from "../../../../components/button/button"
+import {usePatients} from "../../../patients/queries/usePatients"
 export default function FinanceAdd(){
+   const patients = usePatients();
     return(
         <div className="financeAdd">
             <div className="financeAdd-Control">
                 <ListButton label={"اضافة دين"}/>
                 <ListButton label={"استلام دفعة"}/>
             </div>
-            <Select label={"المريض"}/>
+            <Select patients={patients?.data} label={"المريض"}/>
             <Input type="number" label={"المبلغ"}/>
             <Input label={"ملاحظة"}/>
 
--- frontend/src/modules/finance/sections/financeTable/financeTable.jsx
+++ frontend/src/modules/finance/sections/financeTable/financeTable.jsx
@@ -2,71 +2,42 @@ import "./financeTable.css";
 import Search from "../../../../components/search/search";
 import ListButton from "../../../../components/listButton/listButton";
 import Table from "../../../../components/table/table";
+import { useGetFinance } from "../../queries/useGetFinance";
+import { useQueryUI } from "../../../../hooks/useQueryUI";
+import { formatDate } from "../../../../helperFunctions/formatDate";
+import { useState } from "react";
 export default function FinanceTable() {
+const [financeState,setFinanceState] = useState("charges")
+const finance = useGetFinance();
+
+useQueryUI(finance);
+let data = finance.data?.charges ?? [];
+
+if(financeState == "payment"){
+data = finance.data?.payment ?? [];
+}
+
+//functions
+function handleFinanceState(){
+  financeState == "payment"? setFinanceState("charges") : setFinanceState("payment");
+}
+//
   const columns = [
-    { name: "name", label: "الاسم" },
-    { name: "fathar", label: "الاب" },
-    { name: "s_name", label: "العائلة" },
-    { name: "amount", label: "الكمية" },
-    { name: "date", label: "التاريخ" },
+    { name: (finance)=>{
+      return `${finance.patient.name} ${finance.patient.father_name} ${finance.patient.nick_name}`;
+    },
+     label: "الاسم" },
+    { name: "amount", label: "المبلغ" },
+    { name: (finance)=>formatDate(finance.created_at), label: "التاريخ" },
     { name: "note", label: "ملاحظة" },
   ];
-  const data = [
-    {
-      name: "يحيى",
-      fathar: "محمد",
-      s_name: "الحمود",
-      amount: "120$",
-      date: "2026/9/18",
-      note: "دفعة من اجل معالجة ",
-    },
-    {
-      name: "يحيى",
-      fathar: "محمد",
-      s_name: "الحمود",
-      amount: "120$",
-      date: "2026/9/18",
-      note: "دفعة من اجل معالجة ",
-    },
-    {
-      name: "يحيى",
-      fathar: "محمد",
-      s_name: "الحمود",
-      amount: "120$",
-      date: "2026/9/18",
-      note: "دفعة من اجل معالجة ",
-    },
-    {
-      name: "يحيى",
-      fathar: "محمد",
-      s_name: "الحمود",
-      amount: "120$",
-      date: "2026/9/18",
-      note: "دفعة من اجل معالجة ",
-    },
-    {
-      name: "يحيى",
-      fathar: "محمد",
-      s_name: "الحمود",
-      amount: "120$",
-      date: "2026/9/18",
-      note: "دفعة من اجل معالجة ",
-    },
-    {
-      name: "يحيى",
-      fathar: "محمد",
-      s_name: "الحمود",
-      amount: "120$",
-      date: "2026/9/18",
-      note: "دفعة من اجل معالجة ",
-    },
-  ];
+
   return (
     <div className="financeTable">
       <section className="section-1">
         <div className="financeTable-buts">
-          <ListButton label={"الديون"} />
-          <ListButton label={"الدفعات"} />
+          <ListButton onClick={handleFinanceState} selected={financeState == "charges"} label={"الديون"} />
+          <ListButton onClick={handleFinanceState} selected={financeState == "payment"} label={"الدفعات"} />
         </div>
         <Search />
       </section>
